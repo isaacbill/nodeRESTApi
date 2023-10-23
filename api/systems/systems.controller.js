e6ff8c31a83509583f5881019a39bc4ea18f54e4
isaac bill <isaacbill247@gmail.com>
2023-10-23 11:55:48 +0300

    count resolution by system done

--- api/systems/systems.controller.js
+++ api/systems/systems.controller.js
@@ -30,5 +30,61 @@ module.exports={
             });
         });
     },
+    findByPendingStatus: (req, res) => {
+        const systemName = req.params.systemName; // Assuming you're receiving systemName as a parameter
+        const staId = 'pending'; // Assuming you're searching for open status
+
+        findOrderBySystemName(staId, (error, results) => {
+            if (error) {
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+            res.json({
+                success: 1,
+                data: results,
+            });
+        });
+    },
+    findByNewStatus: (req, res) => {
+        const systemName = req.params.systemName; // Assuming you're receiving systemName as a parameter
+        const staId = 'new'; // Assuming you're searching for open status
+
+        findOrderBySystemName(staId, (error, results) => {
+            if (error) {
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+            res.json({
+                success: 1,
+                data: results,
+            });
+        });
+    },
+    findByClosedStatus: (req, res) => {
+        const systemName = req.params.systemName; // Assuming you're receiving systemName as a parameter
+        const staId = 'closed'; // Assuming you're searching for open status
+
+        findOrderBySystemName(staId, (error, results) => {
+            if (error) {
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+            res.json({
+                success: 1,
+                data: results,
+            });
+        });
+    },
+    findByOverdueStatus: (req, res) => {
+        const systemName = req.params.systemName; // Assuming you're receiving systemName as a parameter
+        const staId = 'overdue'; // Assuming you're searching for open status
+
+        findOrderBySystemName(staId, (error, results) => {
+            if (error) {
+                return res.status(500).json({ error: 'Internal Server Error' });
+            }
+            res.json({
+                success: 1,
+                data: results,
+            });
+        });
+    },
     
 };
--- api/systems/systems.router.js
+++ api/systems/systems.router.js
@@ -1,6 +1,11 @@
 const{
     getOrderBySystems, 
-    findByOpenStatus
+    findByOpenStatus,
+    findByNewStatus,
+    findByPendingStatus,
+    findByOverdueStatus,
+    findByClosedStatus
+
 } = require('./systems.controller');
 
 const router= require("express").Router();
@@ -8,5 +13,8 @@ const {checkToken} = require("../../auth/token_validation");
 
 router.get("/",checkToken,getOrderBySystems);
 router.get("/resolution-by-system/open/:systemName",checkToken,findByOpenStatus);
-
+router.get("/resolution-by-system/new/:systemName",checkToken,findByNewStatus);
+router.get("/resolution-by-system/closed/:systemName",checkToken,findByClosedStatus);
+router.get("/resolution-by-system/pending/:systemName",checkToken,findByPendingStatus);
+router.get("/resolution-by-system/overdue/:systemName",checkToken,findByOverdueStatus);
 module.exports =router;
